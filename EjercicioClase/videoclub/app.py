162249c027272d5e94383bcd617b8de6e13e5166
Ad Quinti <quintidani2@gmail.com>
2026-05-16 10:13:34 +0200

    actualizacion json

--- EjercicioClase/videoclub/app.py
+++ EjercicioClase/videoclub/app.py
@@ -1,17 +1,20 @@
 # IMPORTAMOS FLASK
 from flask import Flask, render_template
 import json
+from pathlib import Path
 
 # CREAMOS LA APLICACIÓN
 app = Flask(__name__)
 
+BASE_DIR = Path(__file__).resolve().parent # Obtiene la carpeta donde está guardado 
+
 # RUTA PRINCIPAL
-@app.route("/videoclub")
+@app.route("/videoclub") # http://127.0.0.1:5000/videoclub
 
 def inicio():
+    ruta_json = BASE_DIR / "peliculas.json" # construye la ruta completa al JSON dentro de esa carpeta
     # ABRIMOS EL ARCHIVO JSON
-    with open("peliculas.json", "r", encoding="utf-8") as archivo:
-
+    with open(ruta_json, "r", encoding="utf-8") as archivo:
         # CARGAMOS LOS DATOS JSON
         peliculas = json.load(archivo)
 
@@ -22,7 +25,6 @@ def inicio():
         peliculas=peliculas
     )
 
-# EJECUTAR SERVIDOR
 # EJECUTAR SERVIDOR
 if __name__ == "__main__":
     app.run(debug=True)
